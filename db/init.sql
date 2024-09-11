95d34ff356f78110f6831df9c42cf4f7b97cbf1d
Rodrigo Rios <rodrigo.rios@dinocloudconsulting.com>
2024-09-11 07:34:22 -0300

    feat: new initial commit for new configuration

--- db/init.sql
+++
@@ -1,3 +0,0 @@
--- CREATE DATABASE IF NOT EXISTS codrrdatabase
-SELECT 'CREATE DATABASE codrrdatabase'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'codrrdatabase')\gexec
--- src/libs/apiGateway.lib.ts
+++ src/libs/apiGateway.lib.ts
@@ -1,15 +1,29 @@
-import { HttpStatus } from "../shared/infrastructure/constants/http-status";
+import { HttpStatus } from "../constants/http-status";
+
+export interface ErrorCatch {
+  errorName: string;
+  errorMessage: string;
+  internalErrorCode: string;
+}
+export type lambdaResponse = {
+  statusCode: number;
+  headers: Record<string, unknown>;
+  body: string;
+};
 
 export const formatJSONResponse = <T>({
-    response,
-    statusCode,
-  }: {
-    response: T;
-    statusCode: keyof typeof HttpStatus;
-  }) => {
-    return {
-      statusCode: HttpStatus[statusCode],
-      body: JSON.stringify(response),
-    };
+  response,
+  statusCode,
+}: {
+  response: T;
+  statusCode: keyof typeof HttpStatus;
+}) => {
+  return {
+    statusCode: HttpStatus[statusCode],
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(response),
   };
-  
+};
